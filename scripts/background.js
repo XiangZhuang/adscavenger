b6f0e4bb55da61d2ed1f7e32a8029acf598b337e
XiangZhuang <xzhuangaa@connect.ust.hk>
2022-04-14 00:36:10 +0800

    block ads during runtime

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,23 @@
-chrome.storage.local.get(['adsScavengerOn'], ({ adsScavengerOn }) => {
-  if (adsScavengerOn) {
-    setInterval(() => {
-        [...document.getElementsByClassName("adsbygoogle")].map(element => element.style.display = 'none')
+let timer;
+
+const triggerScavenger = (on) => {
+  if (on) {
+    timer = setInterval(() => {
+      [...document.getElementsByClassName("adsbygoogle")].map(element => element.style.display = 'none')
     }, 1000)
+  } else {
+    if (timer) {
+      clearInterval(timer)
+    }
   }
+}
+
+chrome.storage.local.get(['adsScavengerOn'], ({ adsScavengerOn }) => {
+  triggerScavenger(adsScavengerOn)
 })
+
+chrome.storage.onChanged.addListener((changes) => {
+  const { adsScavengerOn } = changes
+  const { newValue } = adsScavengerOn
+  triggerScavenger(newValue)
+});
